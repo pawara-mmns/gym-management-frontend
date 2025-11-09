0d2470d99b6700e184cca0aeeeda60a6afe6ffee
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-11-09 17:35:30 +0530

    feat: set up routing with BrowserRouter and define routes for Login and Sign Up pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './assets/css/global.css'
+import LoginPage from './pages/loginPage'
+import SignUpPage from './pages/signUpPage'
 
 function App() {
   return (
     <>
-     
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
     </>
   )
 }
-
 export default App
